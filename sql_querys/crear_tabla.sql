d5c57f28f999712b6f7ead65f90587ee045372d8
Taquitoo3000 <taquitodebistek23@gmail.com>
2026-01-15 10:09:59 -0600

    añadir archivo

---
+++ sql_querys/crear_tabla.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Recomendaciones (
+    Consecutivo nvarchar(max), Expediente nvarchar(max), FechaRecom nvarchar(max),
+    Quejoso nvarchar(max), Autoridad nvarchar(max), [Dirigida a] nvarchar(max),
+    Respuesta nvarchar(max), NumRecom nvarchar(max), Recomendacion nvarchar(max),
+    CausaConteo nvarchar(max), Causa nvarchar(max), Efectos nvarchar(max),
+    FechaEfectos nvarchar(max), DSP nvarchar(max), SubProcu nvarchar(max),
+    Observaciones nvarchar(max), FechaRecepcionOficio nvarchar(max),
+    FechaRespuestaOficio nvarchar(max), SGAceptacion nvarchar(max),
+    DerechoManual nvarchar(max), Modalidad nvarchar(max), SGProrroga nvarchar(max),
+    FechaProrroga nvarchar(max), SGCumplimiento nvarchar(max),
+    FechaCumplimiento nvarchar(max), [DerechoInfo2014-generico] nvarchar(max),
+    [DerechoInfo2014-especifico] nvarchar(max), [Respuesta-interna] nvarchar(max),
+    Sentido nvarchar(max)
+);
